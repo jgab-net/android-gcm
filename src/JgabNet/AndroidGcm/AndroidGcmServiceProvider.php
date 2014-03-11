de4de58ab2603b0f6bedd73be22c1f90e77820cb
Abraham José González Barboza <jgab.net@gmail.com>
2014-03-10 21:09:45 -0430

    first commit

---
+++ src/JgabNet/AndroidGcm/AndroidGcmServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace JgabNet\AndroidGcm;
+
+use Illuminate\Support\ServiceProvider;
+
+class AndroidGcmServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
